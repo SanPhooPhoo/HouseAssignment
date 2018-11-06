506a234830c858ab73c6fd6ce80f4e7e4cd84e27
sanphoophoo <sanphooktg@gmail.com>
2018-11-06 23:13:29 +0800

    first commit

---
+++ app/models/house.rb
@@ -0,0 +1,4 @@
+class House < ApplicationRecord
+    validates :Address, :Owner_Name, presence: true, length: { minimum: 5 }
+    has_one_attached :image
+  end
---
+++ app/views/houses/_house.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! house, :id, :number_of_rooms, :Address, :Price, :Owner_Name, :created_at, :updated_at
+json.url house_url(house, format: :json)
